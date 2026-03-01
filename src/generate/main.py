2767c56b19b010bcb605eb52d152e6bd332a74df
DarkCat09 <git@dc09.xyz>
2026-03-01 23:59:28 +0400

    feat(gen): render webpages

--- src/generate/main.py
+++ src/generate/main.py
@@ -5,7 +5,8 @@ from pathlib import Path
 from dataclasses import dataclass
 
 import chatmaild.config
-from jinja2 import Environment
+from jinja2 import Environment, FileSystemLoader
+from markdown import markdown
 
 
 @dataclass
@@ -52,6 +53,52 @@ def render_cfg(gc: GenCfg) -> None:
                 shutil.copy(src, dst)
 
 
+def render_web(gc: GenCfg) -> None:
+    web_src = gc.src_dir / 'web'
+    web_dst = gc.ins_dir / 'web'
+
+    j2_env = Environment(loader=FileSystemLoader(web_src))
+    j2_env.filters['markdown'] = md2html
+    cmd_obj = gc.cmd.__dict__
+
+    for parent, _, files in web_src.walk():
+        parent_rel = parent.relative_to(web_src)
+        parts = parent_rel.parts
+        first_dir = parts[0] if len(parts) > 0 else ''
+        if first_dir == 'layout':
+            continue
+        elif first_dir == 'static':
+            # simply copy files to webroot
+            # removing ./static/ prefix
+            parent_rel = parent_rel.relative_to(first_dir)
+            for file in files:
+                src = web_src / parent / file
+                dst = web_dst / parent_rel / file
+                mkdirs(dst)
+                shutil.copy(src, dst)
+        else:
+            # render as page templates
+            for file in files:
+                tmpl = j2_env.get_template(os.fspath(parent_rel / file))
+                dst = (web_dst / parent_rel / file).with_suffix('')
+                if dst.name == 'index':
+                    dst = dst.with_suffix('.html')
+                else:
+                    dst = dst / 'index.html'
+                mkdirs(dst)
+                with dst.open('wt', encoding='utf-8') as f:
+                    f.write(tmpl.render(**cmd_obj))
+
+
+def md2html(value: str) -> str:
+    return markdown(
+        text=value,
+        extensions=['fenced_code'],
+        output_format='html',
+        tab_length=2,
+    )
+
+
 def mkdirs(p: Path) -> None:
     p.parent.mkdir(parents=True, exist_ok=True)
 
@@ -80,5 +127,6 @@ def init_rundirs(gc: GenCfg) -> None:
 if __name__ == '__main__':
     gc = GenCfg.from_env()
     render_cfg(gc)
+    render_web(gc)
     init_rundirs(gc)
     gc.ready.touch()
