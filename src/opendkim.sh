f50f198c53cf726d031357dceb7cd9ee979e976a
DarkCat09 <git@dc09.xyz>
2026-03-07 16:22:02 +0400

    feat: more useful keygen log line

--- src/opendkim.sh
+++ src/opendkim.sh
@@ -11,7 +11,7 @@ if [ -n "$key_file" ] && [ ! -f "$key_file" ]; then
   selector=$(get_cfg_field Selector)
   /usr/bin/opendkim-genkey -D /etc/dkimkeys -d "$domain" -s "$selector"
   chown -R opendkim: /etc/dkimkeys
-  echo "Created new key: $key_file"
+  echo "Created new key '$key_file' for domain '$domain'"
 fi
 
 exec /usr/sbin/opendkim -u opendkim -f
