f068481c84150ed89886d0662f927995685705df
DarkCat09 <git@dc09.xyz>
2026-03-10 00:15:39 +0400

    feat: parse host from ini for chatmail-turn

--- src/turn.sh
+++ src/turn.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+get_ini_field () {
+  local name="$1"
+  local regex="/^\s*$name\s*=/s/^[^=]+=\s*(\"([^\"]*)\"|'([^']*)'|([^#]*))(\s+#.*)?$/\2\3\4/p"
+  sed -En "$regex" /etc/chatmail.ini | xargs
+}
+
+domain=$(get_ini_field mail_domain)
+
 rundir="/run/chatmail-turn"
 exec /temprundir.sh "$rundir" \
-  /chatmail-turn --realm "$REALM" --socket "$rundir"/turn.socket
+  /chatmail-turn --realm "$domain" --socket "$rundir"/turn.socket
