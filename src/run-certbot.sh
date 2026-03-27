313d2b33e9255e8f6f15cf1abe7c3ce31f67957f
DarkCat09 <git@dc09.xyz>
2026-03-28 00:07:40 +0400

    refactor: set cert deploy hook via cli arg

--- src/run-certbot.sh
+++ src/run-certbot.sh
@@ -2,13 +2,6 @@
 
 . /tls-watch.lib.sh
 
-copy_hook () {
-  if [ "$NO_HOOK" != 1 ]; then
-    echo "Copying deploy hook script"
-    cp /deploy-hook.sh /etc/letsencrypt/renewal-hooks/deploy/chatmail.sh
-  fi
-}
-
 if [ ! -f "$tls_key" ]; then
   mail_domain=$(cat /domain)
   cert_domains="$mail_domain www.$mail_domain mta-sts.$mail_domain $ADD_DOMAINS"
@@ -24,13 +17,12 @@ if [ ! -f "$tls_key" ]; then
   echo "  Domains: $cert_domains"
   echo "  Webroot: $webroot"
 
-  /venv/bin/certbot certonly --webroot -w "$webroot" $arg_domains \
-    || exit $?
-  copy_hook
-  exit 0
+  /venv/bin/certbot certonly \
+    --webroot -w "$webroot" \
+    --deploy-hook /deploy-hook.sh \
+    $arg_domains
+  exit
 fi
-
-copy_hook
 
 mkdir -p /cron
 echo "${CRON_EXPR:-2 1 * * *} /venv/bin/certbot renew -q" >/cron/root
